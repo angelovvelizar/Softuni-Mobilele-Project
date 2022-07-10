1b767ccefb8cf148c42ade12b539c102d906c285
Velizar Angelov <ac0nphantom@gmail.com>
2022-07-10 19:40:52 +0300

    Fixed login errors

--- mobilelele/src/main/java/bg/softuni/web/mobilelele/web/UserLoginController.java
+++ mobilelele/src/main/java/bg/softuni/web/mobilelele/web/UserLoginController.java
@@ -5,14 +5,27 @@ import bg.softuni.web.mobilelele.models.service.UserLoginServiceModel;
 import bg.softuni.web.mobilelele.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UserLoginController {
     @GetMapping("/users/login")
     public String login() {
         return "auth-login";
+    }
+
+    @PostMapping("/users/login-error")
+    public String failedLogin(@ModelAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY) String username,
+                              RedirectAttributes redirectAttributes){
+
+        redirectAttributes.addFlashAttribute("bad_credentials", true);
+        redirectAttributes.addFlashAttribute("username", username);
+
+        return "redirect:/users/login";
     }
 }
